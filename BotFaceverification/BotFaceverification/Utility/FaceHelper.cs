b431f1b5dc070090a1aa0209477a37e765962c1a
Suthahar <jssuthahar@gmail.com>
2018-03-04 22:11:15 +0530

    Cognitive API
    Cognitive API

---
+++ BotFaceverification/BotFaceverification/Utility/FaceHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.ProjectOxford.Face;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BotFaceverification.Utility
+{
+    static class FaceHelper
+    {
+        private static readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("59f7249307ae407f8009d22b35ba2bb0");
+
+        public static async Task<string> UploadAndDetectFaces(string imageFilePath)
+        {
+            try
+            {
+                var requiredFaceAttributes = new FaceAttributeType[] {
+                    FaceAttributeType.Age,
+                    FaceAttributeType.Gender,
+                    FaceAttributeType.Emotion
+                };
+                using (WebClient webClient = new WebClient())
+                {
+                    using (Stream imageFileStream = webClient.OpenRead(imageFilePath))
+                    {
+                        var faces = await faceServiceClient.DetectAsync(imageFileStream, returnFaceLandmarks: true, returnFaceAttributes: requiredFaceAttributes);
+                        var faceAttributes = faces.Select(face => face.FaceAttributes);
+                        string result = string.Empty;
+                        faceAttributes.ToList().ForEach(f =>
+                            result += $"Age: {f.Age.ToString()} Years  Gender: {f.Gender}  Emotion: {f.Emotion.ToString()}{Environment.NewLine}{Environment.NewLine}"
+                        );
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+    }
+}
---
+++ BotObjectDetection/BotObjectDetection/Service/ICaptionService .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BotObjectDetection.Service
+{
+    internal interface ICaptionService
+    {
+        /// <summary> 
+        /// Gets the caption of an image stream. 
+        /// </summary> 
+        /// <param name="stream">The stream to an image.</param> 
+        /// <returns>Description if caption found, null otherwise.</returns> 
+        Task<string> GetCaptionAsync(Stream stream);
+
+        /// <summary> 
+        /// Gets the caption of an image URL. 
+        /// </summary> 
+        /// <param name="url">The URL to an image.</param> 
+        /// <returns>Description if caption found, null otherwise.</returns> 
+        Task<string> GetCaptionAsync(string url);
+    }
+}
